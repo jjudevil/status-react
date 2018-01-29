18a81029991b8ba70f0b11592a1c36dcd3b4b580
alwxndr <alwxndr@gmail.com>
2018-01-29 22:15:11 +0100

    [#3007] Removed location command

---
+++ src/status_im/data_store/realm/schemas/account/v22/core.cljs
@@ -0,0 +1,43 @@
+(ns status-im.data-store.realm.schemas.account.v22.core
+  (:require [status-im.data-store.realm.schemas.account.v19.chat :as chat]
+            [status-im.data-store.realm.schemas.account.v1.chat-contact :as chat-contact]
+            [status-im.data-store.realm.schemas.account.v19.contact :as contact]
+            [status-im.data-store.realm.schemas.account.v20.discover :as discover]
+            [status-im.data-store.realm.schemas.account.v19.message :as message]
+            [status-im.data-store.realm.schemas.account.v12.pending-message :as pending-message]
+            [status-im.data-store.realm.schemas.account.v1.processed-message :as processed-message]
+            [status-im.data-store.realm.schemas.account.v19.request :as request]
+            [status-im.data-store.realm.schemas.account.v19.user-status :as user-status]
+            [status-im.data-store.realm.schemas.account.v5.contact-group :as contact-group]
+            [status-im.data-store.realm.schemas.account.v5.group-contact :as group-contact]
+            [status-im.data-store.realm.schemas.account.v8.local-storage :as local-storage]
+            [taoensso.timbre :as log]
+            [cljs.reader :as reader]
+            [clojure.string :as str]))
+
+(def schema [chat/schema
+             chat-contact/schema
+             contact/schema
+             discover/schema
+             message/schema
+             pending-message/schema
+             processed-message/schema
+             request/schema
+             user-status/schema
+             contact-group/schema
+             group-contact/schema
+             local-storage/schema])
+
+(defn remove-location-messages [old-realm new-realm]
+  (let [messages (.objects new-realm "message")]
+    (dotimes [i (.-length messages)]
+      (let [message (aget messages i)
+            content (aget message "content")
+            type    (aget message "content-type")]
+        (when (and (= type "command")
+                   (> (str/index-of content "command=location") -1))
+          (aset message "show?" false))))))
+
+(defn migration [old-realm new-realm]
+  (log/debug "migrating v21 account database: " old-realm new-realm)
+  (remove-location-messages old-realm new-realm))
